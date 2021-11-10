28217a89a644e6ee132b7f0216e52dda1130f8db
naydin <aydinnecati@gmail.com>
2021-11-10 12:34:08 +0300

    Initial Commit

---
+++ Roomr/RoomrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomrApp.swift
+//  Roomr
+//
+//  Created by Necati Aydın on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct RoomrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
